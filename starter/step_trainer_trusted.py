55cbb00167743752f63acd5c1fb73aa50a1def3e
Dung Dinh <dinhvandung039@gmail.com>
2024-09-01 12:34:43 +0700

    update

--- starter/step_trainer_trusted.py
+++ starter/step_trainer_trusted.py
@@ -4,8 +4,6 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from awsglue import DynamicFrame
-import re
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -14,67 +12,44 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node S3 bucket
-S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "paths": ["s3://human-analytics/customer_curated/"],
-        "recurse": True,
-    },
-    transformation_ctx="S3bucket_node1",
+# Script generated for node Step Trainer Trusted Zone
+StepTrainerTrustedZone_node1679503421946 = (
+    glueContext.create_dynamic_frame.from_catalog(
+        database="stedi",
+        table_name="step_trainer_trusted",
+        transformation_ctx="StepTrainerTrustedZone_node1679503421946",
+    )
 )
 
-# Script generated for node Amazon S3
-AmazonS3_node1692030001186 = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "paths": ["s3://human-analytics/step_trainer_landing/"],
-        "recurse": True,
-    },
-    transformation_ctx="AmazonS3_node1692030001186",
+# Script generated for node Accelerometer Trusted Zone
+AccelerometerTrustedZone_node1 = glueContext.create_dynamic_frame.from_catalog(
+    database="stedi",
+    table_name="accelerometer_trusted",
+    transformation_ctx="AccelerometerTrustedZone_node1",
 )
 
-# Script generated for node Join
-Join_node1692034397757 = Join.apply(
-    frame1=S3bucket_node1,
-    frame2=AmazonS3_node1692030001186,
-    keys1=["serialNumber"],
-    keys2=["serialNumber"],
-    transformation_ctx="Join_node1692034397757",
+# Script generated for node ApplyJoinMapping
+ApplyJoinMapping_node2 = Join.apply(
+    frame1=AccelerometerTrustedZone_node1,
+    frame2=StepTrainerTrustedZone_node1679503421946,
+    keys1=["timestamp"],
+    keys2=["sensorreadingtime"],
+    transformation_ctx="ApplyJoinMapping_node2",
 )
 
 # Script generated for node Drop Fields
-DropFields_node1692034429091 = DropFields.apply(
-    frame=Join_node1692034397757,
-    paths=[
-        "`.serialNumber`",
-        "registrationDate",
-        "lastUpdateDate",
-        "shareWithResearchAsOfDate",
-        "shareWithPublicAsOfDate",
-        "shareWithFriendsAsOfDate",
-        "customerName",
-        "email",
-        "phone",
-        "birthDay",
-    ],
-    transformation_ctx="DropFields_node1692034429091",
+DropFields_node1679503544811 = DropFields.apply(
+    frame=ApplyJoinMapping_node2,
+    paths=["user"],
+    transformation_ctx="DropFields_node1679503544811",
 )
 
-# Script generated for node S3 bucket
-S3bucket_node3 = glueContext.write_dynamic_frame.from_options(
-    frame=DropFields_node1692034429091,
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "path": "s3://human-analytics/step_trainer_trusted/",
-        "partitionKeys": [],
-    },
-    transformation_ctx="S3bucket_node3",
+# Script generated for node Step Trainer Curated Zone
+StepTrainerCuratedZone_node3 = glueContext.write_dynamic_frame.from_catalog(
+    frame=DropFields_node1679503544811,
+    database="stedi",
+    table_name="machine_learning_curated",
+    transformation_ctx="StepTrainerCuratedZone_node3",
 )
 
 job.commit()
